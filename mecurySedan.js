8180973a8b6623b711e8fdc2f3800fa8938c710b
Alex Schmidt <alex.schmidt767@gmail.com>
2022-08-14 14:44:24 -0500

    finished assignment, fogot to commit

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,38 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicleBaseClass");
 
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this,passenger = 0;
+        this.numOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((this.passenger + num) <= this.maxPassengers) {
+                return true;
+            }
+        return false;
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    scheduleService() {
+        if (this.mileage > 30000) {
+            this.scheduleService = true;
+            return this.scheduleService;
+        }
+    }
+}
